53cfae1164f7c7bbe792f1d0996843847cd34be9
fangyuan <fangyuan.yzh@taobao.com>
2016-05-08 17:38:27 +0800

    bucket name 配置化

--- ossUtil.js
+++ ossUtil.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var OSS = require('aliyun-oss');
 var _util = require('./util')._util;
 var url = require('url');
-var domain ; //'http://cdn.bunny-tech.com/'//'http://ossfile.yunos.com/';
-var bucketName = 'bunny-public'//yunosshequ';
+var domain ; 
+//var bucketName; //= 'bunny-public'//yunosshequ';
 
 var ossDir = 'pub/';//oss上文件夹名称
 function getCurrentDir(){
@@ -15,14 +15,17 @@ function getCurrentDir(){
 	return ossDir+y+'-'+m+'/';
 }
 exports.ossUtil= {
-	init:function  (aki,aks,_domain) {
+	init:function  (aki,aks,_domain,bucketName) {
 		option = {
 		  accessKeyId: aki,
-		  accessKeySecret: aks
+		  accessKeySecret: aks,
+		  bucketName:bucketName
 		};
+		
 		domain = _domain;
 	},
-	upload:function (fileName,fn){
+	upload:function (fileName,fn ){
+
 		if (!fileName && !option) {
 			return;
 		};
@@ -40,7 +43,7 @@ exports.ossUtil= {
 	    var oss = OSS.createClient(option) ;
 	    console.log('upload config:',bucketName,saveName,_fileName,JSON.stringify(option))
 	    oss.putObject({
-			  bucket: bucketName,
+			  bucket: option.bucketName,
 			  object: saveName,//保存的完整路径
 			  source: _fileName,//本地文件路径
 			  headers: {
